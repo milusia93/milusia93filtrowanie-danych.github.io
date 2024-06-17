6f0618695b3738b74f3cf8f2dfc407da3e4b8466
MilenaD <44272975+milusia93@users.noreply.github.com>
2024-06-17 15:42:58 +0200

    zadanie skonczone

--- src/UsersList.js
+++ src/UsersList.js
@@ -8,40 +8,61 @@ const UsersList = () => {
         usertype: 'Admin'
     });
 
-    
+    const [users, setUsers] = useState([]);
+
+    const [allUsers, setAllUsers] = useState([]);
+
 
     const handleInputChange = (e) => {
         const target = e.target;
         const name = target.name;
-        setFormData(prevDataForm => {
-            return {...prevDataForm, [name]: target.value}
-        })
+        setFormData(prevDataForm => ({ ...prevDataForm, [name]: target.value }))
     };
-    console.log(formData)
+
+    const setUser = (e) => {
+        e.preventDefault();
+
+        const baseUsers = [...allUsers, { ...formData, id: Date.now() }]
+        setUsers(baseUsers);
+        setAllUsers(baseUsers);
+        console.log(baseUsers);
+    };
+
+    const removeUser = (id) => {
+        const filteredUsers = allUsers.filter(user => user.id !== id)
+        setUsers(filteredUsers);
+        setAllUsers(filteredUsers)
+    };
+
+    const userSelection = (type) => {
+        const selection = allUsers.filter(user => user.usertype === type)
+        setUsers(selection);
+    };
+
+    const showAllUsers = () => {
+        setUsers(allUsers);
+    };
 
     return (
         <div className="usersList">
-            <h2>{formData.username}</h2>
-            <h2>{formData.email}</h2>
-            <h2>{formData.usertype}</h2>
-            <form>
+            <form onSubmit={setUser}>
                 <label htmlFor="username">User name</label>
-                <input 
-                type="text" 
-                id="username" 
-                name="username" 
-                placeholder="User name" 
-                onChange={handleInputChange} 
-                value={formData.username}
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="User name"
+                    onChange={handleInputChange}
+                    value={formData.username}
                 />
                 <label htmlFor="email">User email</label>
-                <input 
-                type="email" 
-                id="email" 
-                name="email" 
-                placeholder="User email" 
-                onChange={handleInputChange}
-                value={formData.email} 
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="User email"
+                    onChange={handleInputChange}
+                    value={formData.email}
                 />
                 <label htmlFor="usertype">User type</label>
                 <select id="usertype" name="usertype" onChange={handleInputChange}>
@@ -50,6 +71,20 @@ const UsersList = () => {
                 </select>
                 <button>Save</button>
             </form>
+            <div className="buttonsPanel">
+                <button onClick={() => userSelection('Admin')}>Wyświetl tylko adminów</button>
+                <button onClick={() => userSelection('User')}>Wyświetl tylko userów</button>
+                <button onClick={showAllUsers}>Wyświetl wszystkich</button>
+            </div>
+            <div className="list">
+                {users.map(user => {
+                    return <div className="userItem" key={user.id} onClick={() => removeUser(user.id)}>
+                        <p>{user.username}</p>
+                        <p>{user.email}</p>
+                        <p>{user.usertype}</p>
+                    </div>
+                })}
+            </div>
         </div>
     );
 };
